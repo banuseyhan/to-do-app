30ca8c65698619d59379843b27b7080c98f62133
Banu Seyhan <banuaskahraman@gmail.com>
2022-12-19 21:26:34 +0300

    style file created

---
+++ todoapp/src/styles/Color.ts
@@ -0,0 +1,6 @@
+const Colors = {
+  defaultYellowColor: 'ffb703', //add button color
+  defaultBlueColor: '8ecae6', //task yazma backgorund color
+  defaultDeepBlueColor: '023047', // ana ekran background color
+};
+export default Colors;
