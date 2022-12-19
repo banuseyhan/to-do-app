1311a04fe3769eae475fffc5615f894a175f28b8
Banu Seyhan <banuaskahraman@gmail.com>
2022-12-19 22:02:39 +0300

    app created

--- todoapp/src/styles/Color.ts
+++ todoapp/src/styles/Color.ts
@@ -1,6 +1,7 @@
 const Colors = {
-  defaultYellowColor: 'ffb703', //add button color
-  defaultBlueColor: '8ecae6', //task yazma backgorund color
-  defaultDeepBlueColor: '023047', // ana ekran background color
+  defaultYellowColor: '#ffb703', //add button color
+  defaultBlueColor: '#8ecae6', //task yazma backgorund color
+  defaultDeepBlueColor: '#023047', // ana ekran background color
+  defaultTitleColor: 'white', //title rengi
 };
 export default Colors;
